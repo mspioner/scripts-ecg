bcfd20daf6a4fc110a39192734062cd98b9c763e
M.Pioner <M.Pioner@PionerMacbook.local>
2019-06-11 20:41:41 -0300

    auth

---
+++ scripts/ssh.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+function person()
+{   
+	pessoa=$f_1
+	ssh -i ~/.ssh/id_rsa user@server "multichain-cli chain_bio liststreamkeyitems stream_bio "${pessoa}" | jq '.[0] .data .json'" > $pessoa
+}
+
+while getopts f: OPCAO;
+do
+	case "${OPCAO}" in
+		f) f_1="${OPTARG}" ;;
+	esac
+done
+
+[ ${f_1} ] && person
+
