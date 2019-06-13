d5577ae7e065b3dc6a510c7b28dae4d34c7f3a06
M.Pioner <M.Pioner@PionerMacbook.local>
2019-06-12 23:44:41 -0300

    atualizado3

--- scripts/gerarJSON_local.sh
+++ scripts/gerarJSON_local.sh
@@ -1,1 +1,2 @@
 #!/usr/bin/env bash
+cat $1 | awk '{print $1,$3}' | sed -n '1,1000p' | sed 's/ /\":\"/g' | sed 's/$/\"/g' |sed 's/ˆ/\"/g' | tr '\n' ',' | sed 's/\",/\",\"/g' | sed 's/,\"$/}/g' | sed 's/^/{\"/'
